d8a9c12a2dea829367378396b1e81c19d05384ab
Ygor Alves <103756299+dev-ygor@users.noreply.github.com>
2022-08-22 10:06:51 -0300

    :recycle: refactor: request in between hr-worker and hr-payroll with Spring Cloud

--- hr-payroll/src/main/java/com/refal/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/refal/hrpayroll/services/PaymentService.java
@@ -1,13 +1,9 @@
 package com.refal.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import com.refal.hrpayroll.FeignClients.WorkerFeignClient;
 import com.refal.hrpayroll.entities.Payment;
 import com.refal.hrpayroll.entities.Worker;
 
@@ -15,19 +11,13 @@ import com.refal.hrpayroll.entities.Worker;
 public class PaymentService {
 	
 	
-	@Value("${hr-worker.host}")
-	private String hrWorkerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, Integer days) {
 		
-		Map<String, String> uriVariables = new HashMap<>();
 		
-		uriVariables.put("id", ""+workerId);
-		
-		Worker worker = restTemplate.getForObject(hrWorkerHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 		
