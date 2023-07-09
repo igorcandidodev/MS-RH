5bb1cc1f11e41b043eb501837837ae213b9b2629
dev-ygor <ygoraveiro@gmail.com>
2023-07-08 21:09:25 -0300

    adicionando dependências novas

--- hr-payroll/src/main/java/com/refal/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/refal/hrpayroll/resources/PaymentResource.java
@@ -10,21 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 import com.refal.hrpayroll.entities.Payment;
 import com.refal.hrpayroll.services.PaymentService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentResource {
-	
+
 	@Autowired
 	private PaymentService service;
-	
+
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable long workerId, @PathVariable Integer days) {
-		
-		
+
 		Payment payment = service.getPayment(workerId, days);
-		
+
 		return ResponseEntity.ok(payment);
-		
+
 	}
+
 
 }
